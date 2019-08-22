c0586f6229b29e00f5e428a678bc4ee21a22b738
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-22 18:48:22 +0200

    Added js switcher mechanizm

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,10 @@
-"use strict";
+const navSwitcher = document.querySelector('.nav__switcher--js')
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+navSwitcher.addEventListener('click', (e) =>{
+  const navigationList = document.querySelector('.nav__text--js');
+  navigationList.classList.toggle('nav__text--visible');
+});
+navSwitcher.addEventListener('click', (e) =>{
+const navigationList = document.querySelector('.mheader__h1--js');
+navigationList.classList.toggle('mheader__h1--padding');
+});
