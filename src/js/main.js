55318efa1502257108fea93c9f5b2f55b7139f13
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-22 19:59:03 +0200

    Updated class name in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-const navSwitcher = document.querySelector('.nav__switcher--js')
+const navSwitcher = document.querySelector('.menu__switcher--js')
 
 navSwitcher.addEventListener('click', (e) =>{
-  const navigationList = document.querySelector('.nav__text--js');
-  navigationList.classList.toggle('nav__text--visible');
-});
-navSwitcher.addEventListener('click', (e) =>{
-const navigationList = document.querySelector('.mheader__h1--js');
-navigationList.classList.toggle('mheader__h1--padding');
+  const navigationList = document.querySelector('.menu__text--js');
+  navigationList.classList.toggle('menu__text--visible');
 });
